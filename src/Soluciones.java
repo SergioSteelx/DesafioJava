257646c7b53eb72869ebbd205cd70cf6306bfaf7
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-04 10:18:50 -0500

    se identa y quitan saltos de linea

--- src/Soluciones.java
+++ src/Soluciones.java
@@ -1,18 +1,12 @@
-
-
-
 public class Soluciones
 {
     public static void main(String[] args)
     {
-
         ProsesamientoDatos data = new ProsesamientoDatos();
-
 
         //String prueba1 = "Hola mundo";
         String prueba1 = "esto es otro ejemplo practico";
 
         data.prosesamientoDatos(prueba1);
-       
     }
 }
