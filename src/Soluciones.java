2daf5a2cb52f56ceb0bce2c5967d27442ed8feab
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-04 13:07:41 -0500

    se conseptualizo  el poderlo implementar con opciones, pero no es funcional de momento

--- src/Soluciones.java
+++ src/Soluciones.java
@@ -10,10 +10,24 @@ public class Soluciones
 
         //String prueba1 = "Hola mundo";
         //String prueba1 = "esto es otro ejemplo practico";
-        System.out.println("Ingrese frase/palabra a prosesar: ");
-        String Cadena = entrada.nextLine();
+        String respuesta;
+        /*System.out.println("Seleccione su opcion:\n0= Usar la prueba por defecto \n1 = Ingresar un texto");
 
-        String respuesta = data.prosesamientoDatos(Cadena);
+        String opcion = entrada.nextLine();
+
+        System.out.println(opcion);
+
+        if("0" == opcion)
+        {
+            String prueba1 = "esto es otro ejemplo practico";
+            respuesta = data.prosesamientoDatos(prueba1);
+        }
+        else
+        {*/
+            System.out.println("Ingrese frase/palabra a prosesar:");
+            String Cadena = entrada.nextLine();
+            respuesta = data.prosesamientoDatos(Cadena);
+        //}
 
         System.out.println(respuesta);
     }
