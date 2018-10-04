22821ac3c2756a7fa7b26f72e038b628a40e041c
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-04 10:15:11 -0500

    Se realizo la migracion de la funcion al archivo desde main, yy se implemento la interface Solucion

--- src/ProsesamientoDatos.java
+++ src/ProsesamientoDatos.java
@@ -1,2 +1,29 @@
-public class ProsesamientoDatos {
+import mx.com.sintelti.desafios.oporx.Solucion;
+
+public class ProsesamientoDatos implements Solucion
+{
+
+    public void prosesamientoDatos(String frace)
+    {
+        int largo = frace.length();
+        String respuesta = "";
+        int j =0;
+        for (int i = 0; i < largo; i++)
+        {
+
+            if (i == frace.indexOf('o',i))
+            {
+                j++;
+                for (int x = 0; x<j; x++){
+                    respuesta=respuesta+"x";
+                }
+            }
+            else
+            {
+                respuesta = respuesta + frace.substring(i,i+1);
+            }
+
+        }
+    }
+
 }
