9ba65940b1c8385c38ebdca8fc96bb08a09e27c1
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-04 12:43:12 -0500

    se le agrego la opcion de poder ingresar la frace/palabra desde el teclado

--- src/Soluciones.java
+++ src/Soluciones.java
@@ -1,13 +1,19 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Soluciones
 {
     public static void main(String[] args)
     {
         ProsesamientoDatos data = new ProsesamientoDatos();
+        Scanner entrada = new Scanner(System.in);
 
         //String prueba1 = "Hola mundo";
-        String prueba1 = "esto es otro ejemplo practico";
+        //String prueba1 = "esto es otro ejemplo practico";
+        System.out.println("Ingrese frase/palabra a prosesar: ");
+        String Cadena = entrada.nextLine();
 
-        String respuesta = data.prosesamientoDatos(prueba1);
+        String respuesta = data.prosesamientoDatos(Cadena);
 
         System.out.println(respuesta);
     }
