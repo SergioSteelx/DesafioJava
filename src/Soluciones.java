a5cc6add36698b5dc892d3088a67fb3d72075561
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-04 10:12:38 -0500

    se muda codigo de main al archivo Prosesamiento de datos y se invoca en la clase main

--- src/Soluciones.java
+++ src/Soluciones.java
@@ -3,39 +3,16 @@
 
 public class Soluciones
 {
-    public static void main(String[] args) implements Solucion()
+    public static void main(String[] args)
     {
 
+        ProsesamientoDatos data = new ProsesamientoDatos();
 
 
         //String prueba1 = "Hola mundo";
         String prueba1 = "esto es otro ejemplo practico";
-        //---- esto regresa la cantidad de o
-        prueba1.indexOf( 'o' );
-        //----  esto indica de donde empieza el texto
-        prueba1.substring(2,6);
-        //---- esto te dice que tan larga es la palabra
-        int largo = prueba1.length();
-        String respuesta = "";
-        int j =0;
-        for (int i = 0; i < largo; i++)
-        {
 
-            if (i == prueba1.indexOf('o',i))
-            {
-                j++;
-                for (int x = 0; x<j; x++){
-                    respuesta=respuesta+"x";
-                }
-            }
-            else
-            {
-                respuesta = respuesta + prueba1.substring(i,i+1);
-            }
-
-        }
-
-        System.out.println(respuesta);
-        System.out.println(prueba1);
+        data.prosesamientoDatos(prueba1);
+       
     }
 }
